b70684faae779d00db1050f27ab83760dd178f18
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-04-24 16:44:05 +0200

    ejr 1

--- lanzador1.py
+++ lanzador1.py
@@ -1,2 +1,25 @@
+import time
+from simulacion_bancaria import Cuenta
 
-    
+def main1():
+    cuenta = Cuenta()
+    print("Actualmente tiene en la cuenta", cuenta.saldo, "€")
+    print("Realizando ingresos y retiradas...")
+    time.sleep(1)
+    for i in range(40):
+        cuenta.ingresar(100)
+    for i in range(20):
+        cuenta.ingresar(50)
+    for i in range(60):
+        cuenta.ingresar(20)
+    for i in range(40):
+        cuenta.retirar(100)
+    for i in range(20):
+        cuenta.retirar(50)
+    for i in range(60):
+        cuenta.retirar(20)
+    print("Ingresos y retiradas realizados")
+    print("Actualmente tiene en la cuenta", cuenta.saldo, "€")
+
+
+main1()
