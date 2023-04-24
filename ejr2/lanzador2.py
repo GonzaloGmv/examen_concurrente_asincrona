3679a34d345d70e0ee4031ce4df1f30e3a10d9cf
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-04-24 17:27:56 +0200

    sin hilos parece q va(falta martingala)

--- ejr2/lanzador2.py
+++ ejr2/lanzador2.py
@@ -3,7 +3,12 @@ from simulador_casino import Ruleta, Jugador
 
 ruleta = Ruleta()
 jugador1 = Jugador()
+jugador2 = Jugador()
 n1 = random.randrange(0,37)
-jugador1.apostar(10, n1, ruleta.lanzar(10, n1))
+n2 = random.randrange(0,37)
+ruleta.lanzar()
+jugador1.apostar_numero(10, n1, ruleta.resultado, ruleta)
+jugador2.apostar_par(10, 0, ruleta.resultado, ruleta)
 print("Saldo del jugador 1: ", jugador1.saldo)
+print("Saldo del jugador 2: ", jugador2.saldo)
 print("Saldo de la ruleta: ", ruleta.saldo)
--- ejr2/simulador_casino.py
+++ ejr2/simulador_casino.py
@@ -1,25 +1,44 @@
 import random
 
 class Ruleta:
-    def __init__(self, saldo=50000):
+    def __init__(self, saldo=50000, resultado=None):
         self.saldo = saldo
+        self.resultado = resultado
+    
+    def lanzar(self):
+        self.resultado = random.randrange(0, 37)
 
-    def lanzar(self, cantidad, numero):
-        resultado = random.randrange(0, 37)
-        if resultado == 0:
+    def numero(self, cantidad, numero):
+        if self.resultado == 0:
             self.saldo += cantidad
-        elif resultado != numero:
+        elif self.resultado != numero:
             self.saldo += cantidad
         else:
             self.saldo -= cantidad * 36
-        return resultado
+        return self.resultado
+    
+    def par(self, cantidad, paridad):
+        if self.resultado == 0:
+            self.saldo += cantidad
+        elif self.resultado%2 != paridad:
+            self.saldo += cantidad
+        else:
+            self.saldo -= cantidad * 2
+        return self.resultado
     
 
 class Jugador:
     def __init__(self, saldo=1000):
         self.saldo = saldo
     
-    def apostar(self, cantidad, numero, resultado):
+    def apostar_numero(self, cantidad, numero, resultado, ruleta):
         self.saldo -= cantidad
         if resultado == numero and resultado != 0:
-            self.saldo += cantidad * 36            
+            self.saldo += cantidad * 36
+        ruleta.numero(cantidad, numero)
+
+    def apostar_par(self, cantidad, paridad, resultado, ruleta):
+        self.saldo -= cantidad
+        if resultado%2 == paridad and resultado != 0:
+            self.saldo += cantidad * 2
+        ruleta.par(cantidad, paridad)
